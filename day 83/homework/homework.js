6bb3456669fb9b2d9408540c3f701d706a480e84
qweqwe1qq <qweqwe@gmail.com>
2025-02-26 06:54:31 -0800

    commit

---
+++ day 83/homework/homework.js
@@ -0,0 +1,14 @@
+let num = 20;
+console.log(num++);
+console.log(--num);
+console.log(++num); 
+console.log(num++);
+
+let age = parseInt(prompt("Enter your age:"));
+age++;
+
+if (age <= 18) {
+    console.log("You are kid");
+} else {
+    console.log("You are adult");
+}
