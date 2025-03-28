4fb98ee038daa794848c72e5538bcc859821951a
zura7cinco7 <tsintsadzezuka11@gmail.com>
2025-03-28 06:17:42 -0700

    second

---
+++ day 03/classwork/noclsswork.py
@@ -0,0 +1,7 @@
+num1 = 10
+num2 = 5
+
+print("მიმატება:", num1 + num2) 
+print("გამოკლება:", num1 - num2) 
+print("გამრავლება:", num1 * num2) 
+print("გაყოფა:", num1 / num2)  
---
+++ day 03/homework/homework.py
@@ -0,0 +1,28 @@
+
+num1 = 0
+while num1 < 10:
+    num1 = num1 + 1
+    print(num1)
+
+num = 20
+while num > 0:
+    num = num - 1
+    print(num)
+
+num2 = 1
+while num2 < 20:
+    num2 = num2 + 2
+    print(num2)
+
+num3 = 0
+while num3 < 100:
+    num3 = num3 + 5
+    print(num3)
+    
+
+
+
+password = input("enter your password:")
+while password != "goa 123":
+    password=input("enter your password:")
+
